408c16f1a207f8ccb1a8eba0f8be90369e8d4508
dionavarastika <diona.varastika@ui.ac.id>
2022-09-14 09:18:18 +0700

    katalog web done

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from katalog.models import CatalogItem
+...
 # TODO: Create your views here.
+
+def show_katalog(request):
+    item_catalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': item_catalog,
+        'nama': 'Diona',
+        'npm': 2106708255,
+    }
+    return render(request, "katalog.html", context)
+
+
