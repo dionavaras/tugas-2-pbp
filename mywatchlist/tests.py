640ecd2769071175add8b02f5c14ab470af830d4
dionavarastika <diona.varastika@ui.ac.id>
2022-09-21 17:04:36 +0700

    tugas 3 done

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -2,15 +2,15 @@ from django.test import TestCase
 from django.urls import reverse
 from mywatchlist.views import show_json, show_xml, show_watchlink
 
-class TestUrls(TestCase):
-    def test_html_url(self):
+class Testing(TestCase):
+    def test_html(self):
         response = self.client.get(reverse("mywatchlist:show_watchlink"))
         self.assertEquals(response.status_code, 200)
 
-    def test_json_url(self):
+    def test_json(self):
         response= self.client.get(reverse("mywatchlist:show_json"))
         self.assertEquals(response.status_code, 200)
 
-    def test_xml_url(self):
+    def test_xml(self):
         response = self.client.get(reverse("mywatchlist:show_xml"))
         self.assertEquals(response.status_code, 200)
